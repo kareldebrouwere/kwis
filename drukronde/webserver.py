9ee9e022a5e1c01941f5fce0cdcbc977808fbb74
kareldebrouwere <kareldebrouwere@gmail.com>
2017-03-02 14:50:14 +0100

    Initial commit

--- drukronde/webserver.py
+++ drukronde/webserver.py
@@ -50,7 +50,7 @@ class Quiz(object):
     @cherrypy.expose
     def displayRed(self):
         print ("In the red button loop")
-        self.pageHTML = self.HTML.format(pushbutton1=self.buttonRed,pushbutton2=self.buttonGrey,pushbutton3=self.buttonGrey,action="")
+        self.pageHTML = self.HTML.format(pushbutton1=self.buttonRed,pushbutton2=self.buttonGrey,pushbutton3=self.buttonGrey,action="self.action")
         self.index()
 
     @cherrypy.expose
