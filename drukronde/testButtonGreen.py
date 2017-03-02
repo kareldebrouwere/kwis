0a98ebcb9e0af164e4439abd2f6b0b7785980d6f
kareldebrouwere <kareldebrouwere@gmail.com>
2017-03-02 16:08:04 +0100

    Initial commit

--- drukronde/testButtonGreen.py
+++ drukronde/testButtonGreen.py
@@ -3,20 +3,20 @@ import time
 import os
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
-    if (GPIO.input(17)) == 0:
+    if (GPIO.input(2)) == 0:
         buttonPressed = 'red'
         # os.system('mpg321 buzzer.mp3 &')
         print('red')
-    elif (GPIO.input(18)) == 0:
+    elif (GPIO.input(3)) == 0:
         buttonPressed = 'green'
         # os.system('mpg321 buzzer.mp3 &')
         print('green')
-    elif (GPIO.input(21)) == 0:
+    elif (GPIO.input(4)) == 0:
         buttonPressed = 'yellow'
         # os.system('mpg321 buzzer.mp3 &')
         print('yellow')
