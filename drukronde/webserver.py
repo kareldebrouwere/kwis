fbb9808b153bcd9049646fd0b9ae69d0c8cde3dd
kareldebrouwere <kareldebrouwere@gmail.com>
2017-03-02 11:53:02 +0100

    Initial commit

--- drukronde/webserver.py
+++ drukronde/webserver.py
@@ -25,3 +25,8 @@ class Quiz(object):
     @cherrypy.expose
     def index(self):
         return HTML.format(button=self.buttonRed,actionForRed="ternRedOn")
+
+
+if __name__ == "__main__":
+    config = os.path.join(os.path.dirname(__file__),'cherrypy.conf')
+    cherrypy.quickstart(QUIZ(),config = config)
