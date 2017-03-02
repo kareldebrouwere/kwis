d4ddc271528bdb56a5ae41416b8301110ff8cf56
kareldebrouwere <kareldebrouwere@gmail.com>
2017-03-02 14:38:57 +0100

    Initial commit

--- drukronde/webserver.py
+++ drukronde/webserver.py
@@ -19,23 +19,38 @@ HTML = """<html>
 
 class Quiz(object):
     def __init__(self):
+        self.HTML = """<html>
+                  <head></head>
+                  <body>
+                    <form>
+                      {button1}
+                      {button2}
+                      {button3}
+                    </form>
+                    <form>
+                      {action}
+                    </form>
+                </body>
+                </html>"""
+        self.HTML.format(button1=self.buttonGrey,button2=self.buttonGrey,button3=self.buttonGrey,action=self.action)
         self.buttonRed = """<img src="/static/buttonRed.jpg" height="100" width="100">"""
         self.buttonBlue = """<img src="/static/buttonBlue.jpg" height="100" width="100">"""
         self.buttonGrey = """<img src="/static/buttonGrey.jpg" height="100" width="100">"""
         self.buttonGreen = """<img src="/static/buttonGreen.png" height="100" width="100">"""
         self.action = """<form method="get" action="displayRed">
-            <button type="submit">Toggle red LED</button>
+            <button type="submit">Start</button>
             </form>
             """
 
     @cherrypy.expose
     def index(self):
-        return HTML.format(button1=self.buttonGrey,button2=self.buttonGrey,button3=self.buttonGrey,action=self.action)
+        return self.HTML
 
     @cherrypy.expose
     def displayRed(self):
         print ("In the red button loop")
-        return HTML.format(button1=self.buttonRed,button2=self.buttonGrey,button3=self.buttonGrey)
+        self.HTML.format(button1=self.buttonRed,button2=self.buttonGrey,button3=self.buttonGrey)
+        self.index()
 
     @cherrypy.expose
     def displayBlue(self):
