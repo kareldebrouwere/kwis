03af6edef29899252ed473304d66c461b83b6f1e
kareldebrouwere <kareldebrouwere@gmail.com>
2017-03-02 15:45:05 +0100

    Initial commit

--- drukronde/webserver.py
+++ drukronde/webserver.py
@@ -1,7 +1,10 @@
-import cherrypy
-import myListenThread
-
 import os
+
+import cherrypy
+
+from drukronde import myListenThread
+
+import time
 
 HTML = """<html>
           <head></head>
@@ -56,7 +59,7 @@ class Quiz(object):
 
     @cherrypy.expose
     def start(self):
-        newListenThread=myListenThread.myListenThread()
+        newListenThread= myListenThread.myListenThread()
         while newListenThread.buttonPressed==None:
             print("nothing happens")
             time.sleep(1)
